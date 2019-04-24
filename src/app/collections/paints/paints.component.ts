edec367faf9aa26a23ea5f15a124638853bba21b
Kelsey Lorenz <kelseylorenz@Kelseys-MacBook-Pro-3.local>
2019-04-24 10:51:17 -0500

    adding more stuff

---
+++ src/app/collections/paints/paints.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-paints',
+  templateUrl: './paints.component.html',
+  styleUrls: ['./paints.component.scss'],
+})
+export class PaintsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
