5648c75aa6e4497cbf5c47bb9db005cd00d58b33
hermaa57 <44037245+hermaa57@users.noreply.github.com>
2019-05-13 09:27:20 -0500

    working like button

--- src/app/social/social-feed/postmodal/postmodal.component.ts
+++ src/app/social/social-feed/postmodal/postmodal.component.ts
@@ -9,15 +9,22 @@ import { ModalController, NavParams } from '@ionic/angular';
 export class PostModalComponent implements OnInit {
 
   public post;
+  private isenabled: boolean;
 
   constructor(private navParams: NavParams, private modCtrl: ModalController) { 
     this.post = this.navParams.get('post');
+    this.isenabled = true;
   }
 
   ngOnInit() {}
 
   dismissModal(){
     this.modCtrl.dismiss();
+  }
+
+  upLikes(){
+    this.post.likes = this.post.likes  + 1;
+    this.isenabled = false;
   }
 
 }
